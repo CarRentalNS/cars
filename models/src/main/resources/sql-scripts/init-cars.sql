ef9f0b4037bebde9761ba65d2e3861fb0048fbb4
Nemanja <soldo.nemanja@gmail.com>
2019-01-15 22:21:23 +0100

    init

--- models/src/main/resources/sql-scripts/init-cars.sql
+++ models/src/main/resources/sql-scripts/init-cars.sql
@@ -1,4 +1,4 @@
-INSERT INTO cars (carLocation, carModel ,carStatus) VALUES ('Ljubljana',"VW Polo","1");
-INSERT INTO cars (carLocation, carModel ,carStatus) VALUES ('Portoroz',"VW Golf","0");
-INSERT INTO cars (carLocation, carModel ,carStatus) VALUES ('Kranj',"Skoda Octavia","1");
+INSERT INTO cars (carLocation, carModel ,carStatus) VALUES ('Ljubljana','VW Polo','1');
+INSERT INTO cars (carLocation, carModel ,carStatus) VALUES ('Portoroz','VW Golf','0');
+INSERT INTO cars (carLocation, carModel ,carStatus) VALUES ('Kranj','Skoda Octavia','1');
 
